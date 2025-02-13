1c63c8aef515216fcf719ce63abbe9047a49f5ee
Mara Mae <avila.maramae.orbeta@gmail.com>
2025-02-13 13:32:15 +0800

    1st commit

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Barangay Old Capitol</title>
+</head>
+<body>
+
+<?php
+    echo "<p>Welcome</p>";
+
+    ?>
+
+</body> 
+</html>
